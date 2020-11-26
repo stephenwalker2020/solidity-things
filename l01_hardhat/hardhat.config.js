2c246899d64d8390c4369dc9a49c62b19379c546
stephenwalker2020 <helenzjoneso5298@gmail.com>
2020-11-27 01:38:04 +0800

    hardhat init

---
+++ l01_hardhat/hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomiclabs/hardhat-waffle");
+
+const {
+  mnemonic,
+  projectId,
+  etherscanKey
+} = require("./secret.json")
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${projectId}`,
+      accounts: {
+        mnemonic: mnemonic,
+      }
+    }
+  }
+};
