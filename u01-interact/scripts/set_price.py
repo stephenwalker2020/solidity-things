f9e461e29e2fb4e49c0608c76e20aa5050126900
stephenwalker2020 <helenzjoneso5298@gmail.com>
2020-12-22 13:03:51 +0800

    update set price

--- u01-interact/scripts/set_price.py
+++ u01-interact/scripts/set_price.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 
 from brownie import accounts, Contract
-from brownie.convert.datatypes import UNITS
 from brownie.network.gas.strategies import GasNowScalingStrategy
 from brownie.network.web3 import Web3
-from brownie.convert.datatypes import HexString
+from pycoingecko import CoinGeckoAPI
 
 gas_strategy = GasNowScalingStrategy("standard", "fast")
 
+def get_prices(symbols):
+    prices = []
+    cg = CoinGeckoAPI()
+    symbol_ids = list(filter(lambda c: c['symbol'].upper() in symbols, cg.get_coins_list()))
+    print(symbol_ids)
+    for s in symbols:
+        coin = list(filter(lambda c: c['symbol'].upper()==s, symbol_ids))
+        assert len(coin) == 1
+        price = cg.get_price(coin[0]['id'], vs_currencies='usd')[coin[0]['id']]['usd']
+        prices.append(str(price))
+    print(prices)
+    return prices
+
 def set():
-    btcPrice = "22724"
-    hbtcPrice = "22478"
-    borPrice = "275.33"
-    wethPrice = "610.06"
-    usdtPrice = "1"
-    usdcPrice = "1"
-    daiPrice = "1"
-    yfiPrice = "24577"
-    yfiiPrice = "1613.25"
-    linkPrice = "12.46"
-    bandPrice = "6.2"
-    nestPrice = "0.034"
     symbols_str = ['BTC', 'HBTC', 'BOR', 'WETH', 'USDT', 'USDC', 'DAI', 'YFI', 'YFII', 'LINK', 'BAND', 'NEST']
     symbols = list(map(lambda s: Web3.toHex(text=s).ljust(66, '0'), symbols_str))
     # print(symbols)
-    prices_str = [btcPrice, hbtcPrice, borPrice, wethPrice, usdtPrice, usdcPrice, daiPrice, yfiPrice, yfiiPrice, linkPrice, bandPrice, nestPrice]
+    prices_str = get_prices(symbols_str)
     prices = list(map(lambda p: Web3.toWei(p, 'ether'), prices_str))
     # print(prices)
     if len(symbols) != len(prices):
